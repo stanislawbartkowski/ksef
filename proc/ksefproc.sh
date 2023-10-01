941cc8292a5cc948304ee3f85972af43f09d5a3a
Stanisław Bartkowski <stanislawbartkowski@gmail.com>
2023-10-01 21:59:52 +0000

    changes

--- proc/ksefproc.sh
+++ proc/ksefproc.sh
@@ -3,7 +3,35 @@
 # ------------------------------------
 # 2023/09/24 - first commit
 
-CURLOUT=`crtemp`
+OK="OK"
+ERROR="ERROR"
+
+# ----------------
+# journal
+# ----------------
+
+OPERATIONLEN=30
+TIMELEN=10
+RESULTLEN=10
+OPISLEN=50
+
+# Adds entry to journal
+# $2 : operation name
+# $3 : starttime
+# $4 : endtime
+# $5 : result
+# $6 : opis
+function journallog() {
+  printreportline $REPFILE "$1" $OPERATIONLEN "$2" $TIMELEN "$3" $TIMELEN "$4" $RESULTLEN "$5" $OPISLEN
+}
+
+function journallognocomment() {
+    printreportline $REPFILE "$1" $OPERATIONLEN "$2" $TIMELEN "$3" $TIMELEN "$4" $RESULTLEN " " $OPISLEN
+}
+
+# ------------------
+# helpers
+# ------------------
 
 buildauthorizationchallengejson() {
     local -r IN=patterns/authorisationchallenge.json
@@ -59,14 +87,6 @@ gethttpcode() {
     echo $HTTP
 }
 
-analizehttpcode() {
-    local -r HTTP=`gethttpcode $1`
-    local -r EXPECTED=$2
-    logfile $1
-    echo $HTTP
-    [ "$HTTP" == "$EXPECTED" ] || logfail "Obtained HTTP code $HTTP  Expected HTTP code $EXPECTED"
-}
-
 getstatusexceptioncode() {
     echo `jq -r .exception.exceptionDetailList[0].exceptionCode $1`
 }
@@ -79,12 +99,6 @@ getinvoicereference() {
     echo `jq -r .elementReferenceNumber $1`
 }
 
-checkstatus() {
-    [ $1 -eq 0 ] && return
-    logfile $2
-    logfail "$3"
-}
-
 
 # create payload for invoice/send
 # $1 - < invoice XML
@@ -93,12 +107,64 @@ createinvoicesend() {
     local -r PATTERN=patterns/invoice.json
     local -r FILESIZE=`cat $1 | wc -c`
     local -r BODY=`cat $1 | base64 -w 0`
+
     #local -r HASH=`md5sum $1 | cut -d " " -f 1 | base64 -w 0`
     local -r HASH=`cat $1 | openssl sha256 -binary | base64 -w 0`
 
     local -r CMD="sed \"s#__HASH__#$HASH#\" $PATTERN"
     local -r CMD1="sed \"s#__INVOICE__#$BODY#\""
     eval $CMD | eval $CMD1 | sed "s/9999999999/$FILESIZE/" >$2
+}
+
+# error handling
+
+# $1 - < file with command output
+# $2 - < expected HTTP code
+# $3 - < OP
+# $4 - < BEG
+# $5 - < (optional) additional file to log
+analizehttpcode() {
+    local -r HTTP=`gethttpcode $1`
+    local -r EXPECTED=$2
+    logfile $1
+    if [ "$HTTP" != "$EXPECTED" ]; then
+        local -r END=`getdate`
+        local -r MESS="Obtained HTTP code $HTTP  Expected HTTP code $EXPECTED"
+        journallog "$3" "$4" "$END" $ERROR "$MESS"
+        echo "xxxxxx $5 xxxx"
+        [ ! -z "$5" ] && logfile $5
+        logfail "$MESS"
+    fi
+}
+
+# $1 - < exist code
+# $2 - < file with command output
+# $3 - < Fail message
+# $4 - < OP
+# $5 - < BEG
+checkstatus() {
+    if [ $1 -ne 0 ]; then
+        local -r END=`getdate`
+        journallog "$4" "$5" "$END" $ERROR "$3"
+        logfile $2
+        logfail "$3"
+    fi
+}
+
+# $1 - < $?
+# $2 - < file with output
+# $3 - < Expected HTTP code
+# $4 - < Fail message
+# $5 - < OP
+# $6 - < BEG
+# $7 - < (optional) additional file to log
+
+verifyresult() {
+    checkstatus $1 $2 "$4" "$5" "$6"
+    analizehttpcode $2 $3 "$5" "$6" $7
+    local -r END=`getdate`
+    [ ! -z "$7" ] && logfile $7
+    journallognocomment "$5" "$6" "$END" $OK    
 }
 
 # -------------------------
@@ -108,13 +174,13 @@ createinvoicesend() {
 # /api/online/Session/AuthorisationChallenge
 # $1 - result
 requestchallenge() {
-    local -r TEMP="/tmp/c.json" 
+    local -r TEMP=`crtemp`
     buildauthorizationchallengejson >$TEMP
+    local -r BEG=`getdate`
     log "Obtaining authorization challenge"
+    local -r OP="Session/AuthorisationChallenge"
     curl $PREFIXURL/api/online/Session/AuthorisationChallenge -d @$TEMP -v -H "Content-Type: application/json" -o $1 >$CURLOUT 2>&1
-    [ $? -eq 0 ] || logfail "Failed to obtain authorization challenge"
-    logfile $1
-    analizehttpcode $CURLOUT 201
+    verifyresult $? $CURLOUT 201 "Failed to obtain authorization challenge" "$OP" "$BEG" $1
 }
 
 # /api/online/Session/InitToken 
@@ -122,10 +188,10 @@ requestchallenge() {
 # $2 - result 
 requestinittoken() {
     log "Obtaining init token"
+    local -r BEG=`getdate`
+    local -r OP="Session/InitToken"
     curl $PREFIXURL/api/online/Session/InitToken -v  -H "Content-Type: application/octet-stream" -H "accept: application/json" -d@$1 -o $2 >$CURLOUT 2>&1
-    [ $? -eq 0 ] || logfail "Failed to obtain init token"
-    logfile $2
-    analizehttpcode $CURLOUT 201
+    verifyresult $? $CURLOUT 201 "Failed to obtain init token" "$OP" "$BEG" $2
 }
 
 # /api/online/Session/Status
@@ -133,10 +199,13 @@ requestinittoken() {
 # $2 - result file
 requestsessionstatus() {
     log "Checking session status"
+    local -r BEG=`getdate`
+    local -r OP="Session/Status"
     local -r SESSIONTOKEN=`getsessiontoken $1`
     [[ ! -z "$SESSIONTOKEN" ]] || logfail "Cannot extract session token"    
     curl $PREFIXURL/api/online/Session/Status -v -H "SessionToken: $SESSIONTOKEN" -o $2  >$CURLOUT 2>&1
-    checkstatus $? $CURLOUT "Failed to verify session status" 
+    checkstatus $? $CURLOUT "Failed to verify session status" "$OP" "$BEG"
+    local -r END=`getdate`
     logfile $2
     logfile $CURLOUT
     local -r HTTPCODE=`gethttpcode $CURLOUT`
@@ -146,42 +215,47 @@ requestsessionstatus() {
       [[ ! -z "$PROCESSINGCODE" ]] || logfail "Cannot extract processing code" 
       [ "$PROCESSINGCODE" == "315" ] && return 0
       [ "$PROCESSINGCODE" == "200" ] && return 1
-      logfail "Unrecognized processing code $PROCESSINGCODE  Expected 315 or 200"
+      local -r MESS="Unrecognized processing code $PROCESSINGCODE  Expected 315 or 200"
+      journallog "$OP" "$BEG" "$END" $ERROR "$MESS"
+      logfail "$MESS"
       ;;
     400) 
         local -r ECODE=`getstatusexceptioncode $2`
         [ "$ECODE" == "21170" ] && return 1
         ;;
     esac
-    logfail "Obtained HTTP code $HTTPCODE, expected 200 or 400"
+    local -r MESS="Obtained HTTP code $HTTPCODE, expected 200 or 400"
+    journallog "$OP" "$BEG" "$END" $ERROR "$MESS"
+    logfail "$MESS"
 }
 
 # /api/online/Session/Terminate
 # $1 - result of the InitToken file, sessiontoken.json
 requestsessionterminate() {
     log "Terminating session"
+    local -r OP="Session/Terminate"
+    local -r BEG=`getdate`
     local -r SESSIONTOKEN=`getsessiontoken $1`
     [[ ! -z "$SESSIONTOKEN" ]] || logfail "Cannot extract session token"
     local -r TEMP=`crtemp`
     curl $PREFIXURL/api/online/Session/Terminate -v -H "SessionToken: $SESSIONTOKEN" -o $TEMP  >$CURLOUT 2>&1
-    checkstatus $? $CURLOUT "Failed to terminate session" 
-    logfile $TEMP
-    analizehttpcode $CURLOUT 200
+    verifyresult $? $CURLOUT 200 "Failed to terminate session" "$OP" "$BEG" $TEMP
 }
 
 # api/online/Invoice/Send
 # $1 - < result of the InitToken file, sessiontoken.json
 # $2 - > request invoice/send 
+# $3 - > invoice status
 directrequestinvoicesend() {
     log "Sending invoice"
+    local -r OP="Invoice/Send"
+    local -r BEG=`getdate`
     local -r SESSIONTOKEN=`getsessiontoken $1`
-    OUT=work/invoicestatus.json
+    local -r OUT=$3
     [[ ! -z "$SESSIONTOKEN" ]] || logfail "Cannot extract session token"
     # Important: PUT
     curl -X PUT $PREFIXURL/api/online/Invoice/Send -v  -H "Content-Type: application/json" -H "accept: application/json" -H "SessionToken: $SESSIONTOKEN" -d@$2 -o $OUT >$CURLOUT 2>&1
-    checkstatus $? $CURLOUT "Failed to send invoice" 
-    logfile $OUT
-    analizehttpcode $CURLOUT 202
+    verifyresult $? $CURLOUT 202 "Failed to send invoice" "$OP" "$BEG" $OUT
 }
 
 
@@ -189,25 +263,24 @@ directrequestinvoicesend() {
 # $1 - < result of the InitToken file, sessiontoken.json
 # $2 - < invoice XML
 # $3 - > request invoice/send
+# $4 - > invoice status
 requestinvoicesend() {
     createinvoicesend $2 $3
-    directrequestinvoicesend $1 $3
+    directrequestinvoicesend $1 $3 $4
 }
 
 
-# (do not use)
 # $1 - < result of the InitToken file, sessiontoken.json
 # $2 - < invoice reference number
 # $3 - > response
 requestreferencestatus() {
     log "Checking invoice reference status $2"
+    local -r OP="Invoice/Status"
+    local -r BEG=`getdate`
     local -r SESSIONTOKEN=`getsessiontoken $1`
     [[ ! -z "$SESSIONTOKEN" ]] || logfail "Cannot extract session token"    
     curl $PREFIXURL/api/online/Invoice/Status/$2 -v -H "SessionToken: $SESSIONTOKEN" -o $3  >$CURLOUT 2>&1
-    checkstatus $? $CURLOUT "Failed to verify invoice reference status" 
-    logfile $3
-    logfile $CURLOUT
-    analizehttpcode $CURLOUT 200
+    verifyresult $? $CURLOUT 200 "Failed to verify invoice reference status" "$OP" "$BEG" $3
 }
 
 # /online/Invoice/Get/{KSeFReferenceNumber}
@@ -239,10 +312,10 @@ requestinvoicesync() {
     analizehttpcode $CURLOUT 200
 }
 
-# /online/Query/Invoice/Async/Init:
+# /online/Query/Invoice/Async/Init
 # $1 < sessiontoken.json
 # $2 < queryjson
-# $3 - > response
+# $3 > response
 requestinvoiceasyncinit() {
     log "Running Query/Invoice/Sync"
     local -r SESSIONTOKEN=`getsessiontoken $1`
@@ -251,6 +324,36 @@ requestinvoiceasyncinit() {
     logfile $3
     logfile $CURLOUT
     analizehttpcode $CURLOUT 202
+}
+
+# ----------------------------------
+# operation
+# ----------------------------------
+
+# invoice send and get ksef reference number
+# $1 < sessiontoken.json
+# $2 < invoice
+# $3 > reference status
+
+requestinvoicesendandreference() {
+    local -r TEMP=`crtemp`
+    local -r ITEMP=`crtemp`
+    requestinvoicesend $1 $2 $TEMP $ITEMP
+    REFERENCENUMBER=`getinvoicereference $ITEMP`
+    local -r OP="Invoice/GetKsefReference"
+    local -r BEG=`getdate`
+    for i in {1..3} 
+    do
+        sleep 5
+        requestreferencestatus $1 $REFERENCENUMBER $3
+        local PCODE=`getprocessingcode $3`
+        [ $PCODE -eq 200 ] && return 0
+        [ $PCODE -ne 310 ] && break
+        log "Invoice still preprocessing, wait 5 sec and retry"
+    done
+    local -r END=`getdate`
+    journallog "$OP" "$BEG" "$END" $ERROR "Cannot extract reference number"
+    logfail "Cannot extract reference number"
 }
 
 # -------------------------------------
@@ -266,10 +369,11 @@ recognizeenv() {
 }
 
 init () {
-    touchlogfile
     usetemp
-    required_listofcommands "openssl base64 jq curl sha256sum"
+    touchlogfile
+    required_listofcommands "openssl base64 jq curl sha256sum uuidgen xmllint"
     recognizeenv
+    export CURLOUT=`crtemp`
 }
 
 init
--- template/test.sh
+++ template/test.sh
@@ -1,37 +1,55 @@
 export TEMPDIR=/tmp/ksef
 export LOGFILE=$TEMPDIR/ksef.log
+export REPFILE=$TEMPDIR/report.txt
 
 . ./env.rc
 . proc/commonproc.sh
 . proc/ksefproc.sh
 
-TIMES=2023-09-22T21:27:52.300Z
-CHALLENGE=20230922-CR-B434B8BBDB-872A019ADD-E1
 
-#createatuhorizedtoken $TIMES
+test() {
+  TIMES=2023-09-22T21:27:52.300Z
+  CHALLENGE=20230922-CR-B434B8BBDB-872A019ADD-E1
 
-#buildinittokenxml $CHALLENGE $TIMES
+  #createatuhorizedtoken $TIMES
 
-#TEST=test/testchallenge.json
-#gettimestampfromchallenge $TEST
-#getchallengefromchallenge $TEST
+  #buildinittokenxml $CHALLENGE $TIMES
 
-#INITTOKEN=test/testinittoken.json
-#getsessiontoken $INITTOKEN
+  #TEST=test/testchallenge.json
+  #gettimestampfromchallenge $TEST
+  #getchallengefromchallenge $TEST
 
-#SESSIONSTATUS=status.json
-#if requestsessionstatus $INITTOKEN $SESSIONSTATUS; then echo "Sesja aktywna"; else echo "Sesja wygasła"; fi
+  #INITTOKEN=test/testinittoken.json
+  #getsessiontoken $INITTOKEN
+
+  #SESSIONSTATUS=status.json
+  #if requestsessionstatus $INITTOKEN $SESSIONSTATUS; then echo "Sesja aktywna"; else echo "Sesja wygasła"; fi
 
 
-#getstatusexceptioncode test/status.json
+  #getstatusexceptioncode test/status.json
 
-#directrequestinvoicesend $SESSIONTOKEN $REQUESTINVOICE
-#createinvoicesend $SESSIONTOKEN $REQUESTINVOICE
+  #directrequestinvoicesend $SESSIONTOKEN $REQUESTINVOICE
+  #createinvoicesend $SESSIONTOKEN $REQUESTINVOICE
+}
+
+test1() {
+  echo "test1"
+  #requestchallenge $RECHALLENGE
+  #createinitxmlfromchallenge $RECHALLENGE >$INITTOKEN
+  #requestinittoken $INITTOKEN $SESSIONTOKEN
+}
+
 
 # -----
 RECHALLENGE=work/rchallenge.json
 INITTOKEN=work/inittoken.xml
 SESSIONTOKEN=work/sessiontoken.json
+REQUESTINVOICE=work/invoice.json
+INVOICESTATUS=work/invoicestatus.json
+SESSIONSTATUS=work/sessionstatus.json
+INVOICE=example/Faktura_KSeF.xml
+INVOICE1=example/faktura.xml
+REFERENCESTATUS=work/referencestatus.json
 
 
 initsession() {
@@ -41,35 +59,22 @@ initsession() {
 }
 
 terminatesession() {
-  requestsessionterminate $SESSIONTOKEN    
+  requestsessionterminate $SESSIONTOKEN 
+    
 }
 
-#initsession
+sendinvoice() {
+  local -r I=$1
+  requestinvoicesend $SESSIONTOKEN $I $REQUESTINVOICE $INVOICESTATUS
+}
 
-SESSIONSTATUS=work/sessionstatus.json
+getksefnumber() {
+  REFERENCENUMBER=`getinvoicereference $INVOICESTATUS`
+  echo $REFERENCENUMBER  
+  requestreferencestatus $SESSIONTOKEN $REFERENCENUMBER $REFERENCESTATUS
+}
+
 #if requestsessionstatus $SESSIONTOKEN $SESSIONSTATUS; then echo "Sesja aktywna"; else echo "Sesja wygasła"; fi
-
-#requestsessionterminate $SESSIONTOKEN
-
-INVOICE=example/Faktura_KSeF.xml
-INVOICE1=example/faktura.xml
-#INVOICE=example/faktura.xml
-REQUESTINVOICE=work/invoice.json
-# requestinvoicesend $SESSIONTOKEN $INVOICE $REQUESTINVOICE
-
-
-#directrequestinvoicesend $SESSIONTOKEN $REQUESTINVOICE
-
-# createinvoicesend $INVOICE $REQUESTINVOICE
-
-INVOICESTATUS=work/invoicestatus.json
-
-REFERENCENUMBER=`getinvoicereference $INVOICESTATUS`
-
-REFERENCESTATUS=work/referencestatus.json
-#requestreferencestatus $SESSIONTOKEN $REFERENCENUMBER $REFERENCESTATUS
-
-#requestinvoiceget $SESSIONTOKEN $REFERENCENUMBER $REFERENCESTATUS
 
 QUERY=patterns/initquery.json
 #requestinvoicesync $SESSIONTOKEN $QUERY $REFERENCESTATUS
@@ -79,16 +84,14 @@ QUERY=patterns/initquery.json
 # sesja, wyślij poprawną fakturę, weź nr ksef i zakończ sesje
 
 #initsession
-#requestinvoicesend $SESSIONTOKEN $INVOICE $REQUESTINVOICE
-#requestreferencestatus $SESSIONTOKEN $REFERENCENUMBER $REFERENCESTATUS
+#requestinvoicesendandreference $SESSIONTOKEN $INVOICE $REFERENCESTATUS
 #terminatesession
 
 # --- scenariusz nr 2, wysłaenie niepoprawej faktury
-# sesja, wyślij nie poprawną fakturę
+# sesja, wyślij niepoprawną fakturę
 
 #initsession
-#requestinvoicesend $SESSIONTOKEN $INVOICE1 $REQUESTINVOICE
-#requestreferencestatus $SESSIONTOKEN $REFERENCENUMBER $REFERENCESTATUS
+#requestinvoicesendandreference $SESSIONTOKEN $INVOICE1 $REFERENCESTATUS
 #terminatesession
 
 
